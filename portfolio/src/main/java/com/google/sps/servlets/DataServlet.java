a2d619c1b77ee63b52297cc07b6b91d2d49c274a
Olivia Tobin <oliviatobin@gmail.com>
2020-06-30 18:42:25 +0000

    Adjust to styleguide and fix naming issues

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -20,31 +20,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+import java.util.*;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   
-  private ArrayList<String> movie_quotes;
+  private ArrayList<String> data;
   /* Modify DataServlet so its doGet() function returns the ArrayList as a JSON String. */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
       /** Create a ArrayList<String> variable containing hard coded famous movie quotes */
-    movie_quotes = new ArrayList<>();
-    movie_quotes.add("According to all known laws of aviation, there is no way a bee should be able to fly");
-    movie_quotes.add("This conversation can serve no purpose anymore. Goodbye.");
-    movie_quotes.add("It is the TITULAR role");
-    movie_quotes.add("Honey? Where is my supersuit");
-    String json = convertToJson(movie_quotes);
+    ArrayList<String> movieQuotes = new ArrayList<String>(
+        Arrays.asList("According to all known laws of aviation, there is no way a bee should be able to fly",
+                        "This conversation can serve no purpose anymore. Goodbye.",
+                        "It is the TITULAR role",
+                        "Honey? Where is my supersuit"
+                    )
+    );
+    String json = convertToJson(movieQuotes);
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
   
   /* Convert to JSOn using Gson */
-  private String convertToJson(ArrayList<String> movie_quotes) {
+  private String convertToJson(ArrayList<String> data) {
     Gson gson = new Gson();
-    String json = gson.toJson(movie_quotes);
+    String json = gson.toJson(data);
     return json;
   }
 }
