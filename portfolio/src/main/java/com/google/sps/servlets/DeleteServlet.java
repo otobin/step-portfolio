602a17e986211c8be8ed7fb49e6a3a6d1af265e1
Olivia Tobin <oliviatobin@gmail.com>
2020-07-06 20:12:22 +0000

    minor styling changes

--- portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
@@ -1,13 +1,11 @@
 package com.google.sps.servlets;
 
-import com.google.gson.*;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.KeyRange;
@@ -16,8 +14,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
-import com.google.sps.data.Comment;
 
 
 
