a177fe93fa47bf7311b478e8faa181211dfdf17c
Olivia Tobin <oliviatobin@gmail.com>
2020-07-08 18:05:24 +0000

    Add dropdowns to map locations. Currently not working

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -107,8 +107,14 @@ function deleteAllComments() {
     responsePromise.then(displayComments());
 }
 
-function goToDiv(divIdentity) {
-    var url = "optobin.appspot."
+function revealInformation(paragraphId) {
+    const paragraphElement = document.getElementById(`more-info-${paragraphId}`);
+    if (paragraphElement.style.maxHeight === '0px') {
+        paragraphElement.style.maxHeight = '200px';
+    } else {
+        paragraphElement.style.maxHeight = '0px';
+    }
+
 }
 
 
@@ -118,19 +124,20 @@ function createMap() {
       document.getElementById('map'),
       {center: {lat: 38.5449, lng: -121.7405}, zoom: 14});
   let names = ["Philz Coffee", "Mishka's Cafe", "Peet's Coffee", "Temple Coffee Roasters", "ASUCD Coffee House"];
-  let divIds = ['philz', 'mishkas', 'peets', 'temple', 'asucd'];
+  let paragraphIds = ['philz', 'mishkas', 'peets', 'temple', 'asucd'];
   let coordinates = [ {lat: 38.544331, lng: -121.735481}, 
                     {lat: 38.543150, lng: -121.740530}, 
                     {lat: 38.544110, lng: -121.741800},
                     {lat: 38.544781, lng: -121.739609}, 
                     {lat: 38.542335, lng: -121.749454}];
   
-  // To do: add event listeners for markers to animate dropdown menu
+  
   for (var i = 0; i < names.length; i++) {
       const marker = new google.maps.Marker({
           position: coordinates[i],
           map: davis_map,
           title: names[i]
       });
+      marker.addListener('click', revealInformation(paragraphIds[i]));
   }
 }
